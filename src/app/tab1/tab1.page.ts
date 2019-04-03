2b3ca5389c3acf2d87896d11d79c3fcff036e852
Mark Nambatac <nambatac80@gmail.com>
2019-04-03 23:26:21 +0800

    Initial Commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,134 @@
+import { Component } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+import { AlertController, ToastController, LoadingController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  output: any;
+  message: any;
+  responseText: any;
+  unpairedDevices: any;
+  pairedDevices: any;
+  statusMessage: any;
+  gettingDevices: boolean;
+
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController, 
+    private bluetoothSerial: BluetoothSerial, public loadingController: LoadingController) {
+    bluetoothSerial.enable();
+  }
+
+  startScanning() {
+    this.pairedDevices = null;
+    this.unpairedDevices = null;
+    this.gettingDevices = true;
+
+
+    this.bluetoothSerial.discoverUnpaired().then((success) => {
+      this.unpairedDevices = success;
+      this.gettingDevices = false;
+      success.forEach(element => {
+
+      });
+    }, (err) => {
+      console.log(err);
+    });
+
+    this.bluetoothSerial.list().then((success) => {
+      this.pairedDevices = success;
+    }, (err) => {
+
+    })
+  }
+
+  success = (data) => alert(data);
+  fail = (error) => alert(error);
+
+  async selectDevice(address: any) {
+    try { 
+      const alertButts = await this.alertCtrl.create({
+        header: 'Connect',
+        message: 'Do you want to connect with?',
+        buttons: [{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }, {
+          text: 'Connect',
+          handler: () => {
+            this.bluetoothSerial.connect(address).subscribe(this.success, this.fail);
+            this.data();
+          }
+        }]
+      });
+
+      try {
+        await alertButts.present();
+      } catch(e) {
+        console.log("Error napud haha");
+      }
+    } catch(e) {
+      console.log("Error Occurred");
+    }
+
+   
+  }
+
+
+  async disconnect() {
+    try {
+      const disconnectButt = await this.alertCtrl.create({
+        header: 'Disconnect Device',
+        message: 'Do you want to Disconnect?',
+        buttons: [{
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              disconnectButt.dismiss();
+            }
+          },
+          {
+            text: 'Disconnect',
+            handler: () => {
+              this.bluetoothSerial.disconnect();
+              this.gettingDevices = null;
+            }
+          }
+        ]
+      });
+      try {
+        await disconnectButt.present();
+      }
+      catch(e) {
+        console.log("Error napud");
+      }
+      
+    } catch(e) {
+      console.log("error Occurred");
+    }
+    
+  }
+
+  data() {
+    setInterval(() => {
+      this.read();
+    }, 3000);
+  }
+
+
+  read() {
+    this.bluetoothSerial.readUntil('\n').then((data) => {
+      console.log(`Data : ${data}`);
+      var actual_data = JSON.parse(data);
+      this.output = actual_data;
+      console.log(actual_data.temperature);
+    });
+  }
+
+}
